9d3e63c32c576b5c1b91ad381dd4a6c1597dd5cf
Serhiy Gerych <frodo13lot@gmail.com>
2024-03-26 20:52:38 +0200

    fix slider and scroll baner

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,20 +1,23 @@
-import style from './style.module.scss'
+import { forwardRef } from 'react';
+import style from './style.module.scss';
 
-
-export default function Categories ({categories, selectedCategory, setSelectedCategory}) {
-  return(
-    <div className={style.categories}>
-      <button onClick={() => setSelectedCategory(null)} className={!selectedCategory? style.active : style.item}>
-              {'all'}
-          </button>
-      {
-        categories.map(elem => {
-          
-          return <button key={elem} onClick={() => setSelectedCategory(elem)} className={selectedCategory === elem ? style.active : style.item}>
-              {elem}
-          </button>
-        })
-      }
+const Categories = forwardRef(
+  ({ categories, selectedCategory, setSelectedCategory }, ref) => {
+  return (
+    <div ref={ref} className={style.categories}>
+      <button onClick={() => setSelectedCategory(null)} className={!selectedCategory ? style.active : style.item}>
+        {'all'}
+      </button>
+      {categories.map(elem => (
+        <button key={elem} onClick={() => setSelectedCategory(elem)} className={selectedCategory === elem ? style.active : style.item}>
+          {elem}
+        </button>
+      ))}
     </div>
-  )
-}
+    );  
+  }
+);
+
+Categories.displayName = "Categories";
+
+export default Categories;
